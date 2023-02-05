1986fb5bf89a8d50a80bd374d75ee8378bbb9e14
DavidSahakyan <123825441+DavidSahakyan@users.noreply.github.com>
2023-02-05 13:42:27 +0400

    Add files via upload

--- logger.cpp
+++ logger.cpp
@@ -28,34 +28,46 @@ public:
 
 
     template <typename T>
-    void ilog(T info)
+    void ilog(T info, std::string func_name = "")
     {
         auto date_time = std::chrono::system_clock::now();
         std::time_t log_time = std::chrono::system_clock::to_time_t(date_time);
         this->logfile << std::ctime(&log_time);
         this->logfile << path << std::endl;
+        if(func_name != "")
+        {
+            this->logfile << "PROGRAMM IS IN " << func_name << std::endl;
+        }
         this->logfile<< "INFO"<< ":" << info << std::endl;
         logfile<<"------------------------------------------"<<std::endl;
     }
 
     template <typename T>
-    void elog(T info)
+    void elog(T info, std::string func_name = "")
     {
         auto date_time = std::chrono::system_clock::now();
         std::time_t log_time = std::chrono::system_clock::to_time_t(date_time);
         this->logfile << std::ctime(&log_time);
         this->logfile << path << std::endl;
+        if(func_name != "")
+        {
+             this->logfile << "PROGRAMM IS IN " << func_name << std::endl;
+        }
         this->logfile << "ERROR"<< ":" << info << std::endl;
         logfile<<"------------------------------------------"<<std::endl;
     }
 
-     template <typename T>
-    void wLog(T info)
+    template <typename T>
+    void wLog(T info, std::string func_name = "")
     {
         auto date_time = std::chrono::system_clock::now();
         std::time_t log_time = std::chrono::system_clock::to_time_t(date_time);
         this->logfile << std::ctime(&log_time);
         this->logfile << path << std::endl;
+        if(func_name != "")
+        {
+            this->logfile << "PROGRAMM IS IN " << func_name << std::endl; 
+        }
         this->logfile<< "WARNING"<< ":" << info << std::endl;
         logfile<<"------------------------------------------"<<std::endl;
     }
@@ -75,12 +87,14 @@ private:
 
     char buff[50]; 
     std::string path = getcwd(buff, 100);
+
+    std::string func_name; 
 };
 
 int main()
 {
     Logger file;
-    file.ilog("INFO LOG CREATED");
-    file.elog("ERROR LOG CREATED");
+    file.ilog("INFO LOG CREATED", "some_func");
+    file.elog("ERROR LOG CREATED", "other_func");
     
 }
